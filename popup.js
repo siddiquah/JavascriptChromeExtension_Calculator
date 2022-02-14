35628166c543109114336b49d3c33966c365c7b5
siddiquah anjum <50186478+siddiquah@users.noreply.github.com>
2022-02-14 19:18:44 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,28 @@
+var submitBtn = document.getElementById("submit").addEventListener("click", calculate)
+document.getElementById("7b").addEventListener("click", () => (des(7)))
+document.getElementById("8b").addEventListener("click", () => (des(8)))
+document.getElementById("9b").addEventListener("click", () => (des(9)))
+document.getElementById("4b").addEventListener("click", () => (des(4)))
+document.getElementById("5b").addEventListener("click", () => (des(5)))
+document.getElementById("6b").addEventListener("click", () => (des(6)))
+document.getElementById("1b").addEventListener("click", () => (des(1)))
+document.getElementById("2b").addEventListener("click", () => (des(2)))
+document.getElementById("3b").addEventListener("click", () => (des(3)))
+document.getElementById("0b").addEventListener("click", () => (des(0)))
+document.getElementById("+b").addEventListener("click", () => (des("+")))
+document.getElementById("/b").addEventListener("click", () => (des("/")))
+document.getElementById("-b").addEventListener("click", () => (des("-")))
+document.getElementById("*b").addEventListener("click", () => (des("*")))
+
+const inputField = document.getElementById("textField")
+
+function calculate() {
+    var input = inputField.value
+    const value = eval(input)
+    inputField.value = value
+}
+
+function des(val) {
+    inputField.value += val
+}
+
